cef485d6396dc6de739442f8e82202743519900c
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-04-22 23:58:40 +0200

    fix: stub implementation of v2 client

--- src/FlexportApiClient.ts
+++ src/FlexportApiClient.ts
@@ -1,3 +1,9 @@
+import * as fetchPonyfill from 'fetch-ponyfill'
+import { flexportApiV2 } from './generated/apiClient'
+import { headers } from './headers'
+
+const { fetch } = fetchPonyfill()
+
 export type FlexportApiClient = <A>(args: {
 	path: string
 	method: 'GET' | 'POST' | 'PATCH'
@@ -6,3 +12,29 @@ export type FlexportApiClient = <A>(args: {
 		query?: { [key: string]: any }
 	}
 }) => A
+
+export const v2Client = ({
+	apiKey,
+	endpoint,
+	fetchImplementation,
+}: {
+	apiKey: string
+	endpoint?: string
+	fetchImplementation?: typeof fetch
+}) => {
+	const e = endpoint?.replace(/\/$/, '') || 'https://api.flexport.com'
+	const clientImplementation: FlexportApiClient = ({
+		path,
+		method,
+		params,
+	}) => {
+		const url = `${e}${path}`
+		// TODO: replace path parameters
+		return (fetchImplementation || fetch)(url, {
+			method,
+			headers: headers({ apiKey }),
+			query: params?.query,
+		}).then(async (res: any) => res.json())
+	}
+	return flexportApiV2(clientImplementation)
+}
