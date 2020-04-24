b22cac9e1f9e7e902ad56558adb5e7c13c83226f
Markus Tacker <Markus.Tacker@NordicSemi.no>
2020-04-24 10:33:17 +0200

    fix: use tryCatch

--- src/FlexportApiClient.spec.ts
+++ src/FlexportApiClient.spec.ts
@@ -1,6 +1,8 @@
 import { v2Client } from './FlexportApiClient'
 import { requestHandler } from '@distributeaid/flexport-api-sandbox'
 import * as http from 'http'
+import { pipe } from 'fp-ts/lib/pipeable'
+import * as TE from 'fp-ts/lib/TaskEither'
 
 const port = 3000
 const hostname = `http://0.0.0.0:${port}`
@@ -9,9 +11,7 @@ describe('FlexportApiClient', () => {
 	let server: http.Server
 	beforeAll(() => {
 		server = http.createServer(requestHandler(hostname))
-		server.listen(port, () => {
-			console.debug('[Flexport API Sandbox]', `is listening on ${hostname}`)
-		})
+		server.listen(port)
 	})
 	afterAll(() => {
 		server.close()
@@ -28,7 +28,12 @@ describe('FlexportApiClient', () => {
 			apiKey: 'foo',
 			endpoint: hostname,
 		})
-		const shipments = await client.shipment_index()
-		expect(shipments.data).toHaveLength(10)
+		expect.assertions(1)
+		return pipe(
+			client.shipment_index(),
+			TE.map((shipments) => {
+				expect(shipments.data).toHaveLength(10)
+			}),
+		)()
 	})
 })
--- src/FlexportApiClient.ts
+++ src/FlexportApiClient.ts
@@ -1,17 +1,22 @@
 import * as fetchPonyfill from 'fetch-ponyfill'
 import { flexportApiV2 } from './generated/apiClient'
 import { headers } from './headers'
+import * as TE from 'fp-ts/lib/TaskEither'
+import { ErrorInfo, createError } from './types/ErrorInfo'
 
 const { fetch } = fetchPonyfill()
 
-export type FlexportApiClient = <A>(args: {
+export type FlexportApiClientArgs = {
 	path: string
 	method: 'GET' | 'POST' | 'PATCH'
 	params?: {
 		path?: { [key: string]: any }
 		query?: { [key: string]: any }
 	}
-}) => A
+}
+export type FlexportApiClient = <A>(
+	args: FlexportApiClientArgs,
+) => TE.TaskEither<ErrorInfo, A>
 
 export const v2Client = ({
 	apiKey,
@@ -23,20 +28,25 @@ export const v2Client = ({
 	fetchImplementation?: typeof fetch
 }) => {
 	const e = endpoint?.replace(/\/$/, '') || 'https://api.flexport.com'
-	const clientImplementation: FlexportApiClient = ({
+	const clientImplementation: FlexportApiClient = <A>({
 		path,
 		method,
 		params,
-	}) => {
+	}: FlexportApiClientArgs) => {
+		// FIXME: replace path parameters
+		// FIXME: Handle errors
 		const url = `${e}${path}`
-		// TODO: replace path parameters
-		return (fetchImplementation || fetch)(url, {
-			method,
-			headers: headers({ apiKey }),
-			query: params?.query,
-		})
-			.then(async (res: any) => res.json())
-			.then((res: any) => res.data)
+		return TE.tryCatch<ErrorInfo, A>(
+			() =>
+				(fetchImplementation || fetch)(url, {
+					method,
+					headers: headers({ apiKey }),
+					query: params?.query,
+				})
+					.then(async (res: any) => res.json())
+					.then((res: any) => res.data),
+			(err) => createError((err as Error).message),
+		)
 	}
 	return flexportApiV2(clientImplementation)
 }
